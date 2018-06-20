6eff91edc3c19553aecaa4dad5ce3a0ac251187c
James Cullen <jimcullenaus@gmail.com>
2018-06-20 23:11:35 +1000

    Create city preference

--- app/src/main/java/com/thejimcullen/petrolcycletracker/MainActivity.java
+++ app/src/main/java/com/thejimcullen/petrolcycletracker/MainActivity.java
@@ -145,7 +145,7 @@ public class MainActivity extends AppCompatActivity
 		int id = item.getItemId();
 
 		if (id == R.id.action_settings) {
-			Intent settingsIntent = new Intent(this, SettingsActivity.class);
+			Intent settingsIntent = new Intent(this, PetrolPreferencesActivity.class);
 			startActivity(settingsIntent);
 			return true;
 		}
